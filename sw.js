f950a058e93926891a893b29d57b337ebaa1efa1
Hung Pham <hungpham@Hungs-MacBook-Pro-2.local>
2026-05-04 16:36:22 -0700

    Revert ?heading=N URL param

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
 //   - Responses are validated before being cached so captive portals can't
 //     poison the cache with their HTML.
 
-const SHELL_VERSION  = 'myraverlife-shell-v156';
+const SHELL_VERSION  = 'myraverlife-shell-v157';
 const IMAGES_VERSION = 'myraverlife-images-v1';
 
 const APP_SHELL = [
