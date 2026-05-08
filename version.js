43f533d3bb69ae0553dd08a8a88a7471935369d2
Hung Pham <hungpham@Hungs-MacBook-Pro-2.local>
2026-05-07 22:29:37 -0700

    v248: warmer copy in thanks modal

--- version.js
+++ version.js
@@ -4,4 +4,4 @@
 //
 // `self` is the global in both window contexts and service workers,
 // so this file works for both via `<script src>` and `importScripts()`.
-self.APP_VERSION = 'v247';
+self.APP_VERSION = 'v248';
